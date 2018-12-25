3017f02da9b04167246f20a3151fa423b8d02bdf
wangsen <wangsen961225@163.com>
2018-12-25 17:25:18 +0800

    IDEA springboot+mybatis+poi数据导出Excel系统_v0.1

--- src/main/java/com/zsx/crm_excel/CrmExcelApplication.java
+++ src/main/java/com/zsx/crm_excel/CrmExcelApplication.java
@@ -2,10 +2,11 @@ package com.zsx.crm_excel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan("com.zsx.crm_excel")
 public class CrmExcelApplication {
 
